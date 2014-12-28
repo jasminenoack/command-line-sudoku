3959e1a756588b5e64265892af89e1465848cc87
jasminenoack <jasminenoack@gmail.com>
2014-12-28 18:31:49 -0500

    solves medium puzzzles

--- sudoku.rb
+++ sudoku.rb
@@ -1,12 +1,10 @@
 class Sudoku
   attr_accessor :puzzle, :rotated
 
-  def initialize(puzzle)
-    @puzzle=Array.new(81, "_").each_slice(9).to_a
-    puzzle.each do |key, value|
-      key=key.split("").map{|x| x.to_i}
-      @puzzle[key[0]-1][key[1]-1]=value
-    end
+  def initialize(file)
+    puzzle=File.open(file).read.split("\n")
+    @puzzle=[]
+    puzzle.each{|row| @puzzle << row.split("").map{|num| num=="0" ? "_" : num.to_i}}
   end
 
   def display
@@ -34,82 +32,198 @@ class Sudoku
   end
 
   def check_row(row)
-    row=@puzzle[row-1]
+    row=@puzzle[row]
     check(row)
   end
 
   def check_column(column)
     @rotate=rotate
-    column=@rotated[column-1]
+    column=@rotated[column]
     check(column)
   end
 
   def find_square(row, column)
-    start_h=(((row-1)/3)*3)
-    start_v=(((column-1)/3)*3)
+    start_h=(((row)/3)*3)
+    start_v=(((column)/3)*3)
     square=[]
     (start_h..start_h+2).each do |row|
       (start_v..start_v+2).each do |column|
-        square << @puzzle[row][column]
+        square << [row, column]
       end
     end
     square
   end
 
   def check_square(row, column)
-    square=find_square(row, column)
+    places=find_square(row, column)
+    square=[]
+    places.each {|place| square << @puzzle[place[0]][place[1]]}
     check(square)
   end
 
   def check_place (row, column)
-    return @puzzle[row-1][column-1] if @puzzle[row-1][column-1]!="_"
+    return @puzzle[row][column] if @puzzle[row][column]!="_"
     check_row(row) & check_column(column) & check_square(row, column)
   end
 
   def solve_squares
-    (1..9).each do |row|
-      (1..9).each do |column|
+    (0..8).each do |row|
+      (0..8).each do |column|
         options=check_place(row, column)
-        @puzzle[row-1][column-1]=options[0] if options.class==Array && options.length==1
+        @puzzle[row][column]=options[0] if options.class==Array && options.length==1
       end
     end
     self
   end
 
+  def check_incomplete
+    incomplete=@puzzle.map {|row| row.count("_")}.inject(:+)
+  end
+
+  def find_options(places)
+    options=[]
+    places.each do |place|
+      options << check_place(place[0],place[1])
+    end
+    options
+  end
+
+  def compare (options)
+    nums=options.flatten.sort
+    (1..9).each {|num| nums.delete(num) if nums.count(num)>1}
+    nums
+  end
+
+  def update_from_compare(singles, options, places)
+    singles.each do |num|
+      if options.include?(num)
+        next
+      else
+        options.each_with_index do |nums, index|
+          next if nums.class==Fixnum
+          if nums.include?(num)
+            row = places[index][0]
+            column = places[index][1]
+            @puzzle[row][column]=num
+          end
+        end
+      end
+    end
+  end
+
+  def compare_row(row)
+    rows=Array.new(9, row)
+    columns=(0..8).to_a
+    places=rows.zip(columns)
+    options=find_options(places)
+    singles=compare(options)
+    update_from_compare(singles, options, places)
+  end
+
+  def compare_rows
+    (0..8).each {|num| compare_row(num)}
+  end
+
+  def compare_column(column)
+    columns=Array.new(9, column)
+    rows=(0..8).to_a
+    places=rows.zip(columns)
+    options=find_options(places)
+    singles=compare(options)
+    update_from_compare(singles, options, places)
+  end
+
+  def compare_columns
+    (0..8).each {|num| compare_column(num)}
+  end
+
+  def compare_square(row, column)
+    places = find_square(row, column)
+    options = find_options(places)
+    singles = compare(options)
+    update_from_compare(singles, options, places)
+  end
+
+  def compare_squares
+    (0..2).each do |row|
+      (0..2).each do |column|
+        compare_square(row*3, column*3)
+      end
+    end
+  end
+
+  def complete_puzzle
+    incomplete=check_incomplete
+    while incomplete>0
+      start=incomplete
+      solve_squares
+      incomplete=check_incomplete
+      if incomplete == start
+        compare_squares
+        compare_rows
+        compare_columns
+        incomplete=check_incomplete
+        if incomplete == start
+          break
+        end
+      end
+    end
+    if incomplete > 0
+      puts "I'm unfortunately unable to solve this puzzle"
+      display
+    else
+      puts "I have solved the puzzle"
+      display
+    end
+  end
+
 end
 
+=begin
+#easy
+puzzle = Sudoku.new('puzzle1.txt')
+puzzle.complete_puzzle
+
+puzzle = Sudoku.new('puzzle2.txt')
+puzzle.complete_puzzle
+
+#medium
+puzzle = Sudoku.new('puzzle3.txt')
+puzzle.complete_puzzle
+
+puzzle = Sudoku.new('puzzle4.txt')
+puzzle.complete_puzzle
+=end
+
+#hard
+puzzle = Sudoku.new('puzzle6.txt')
+puzzle.complete_puzzle
+
+puzzle = Sudoku.new('puzzle5.txt')
+puzzle.complete_puzzle
+
+#super hard
+puzzle = Sudoku.new('puzzle7.txt')
+puzzle.complete_puzzle
+
+puzzle = Sudoku.new('puzzle8.txt')
+puzzle.complete_puzzle
+
+puzzle = Sudoku.new('puzzle9.txt')
+puzzle.complete_puzzle
+
+#evil
+=begin
+puzzle = Sudoku.new('puzzle10.txt')
+puzzle.complete_puzzle
+
+puzzle = Sudoku.new('puzzle11.txt')
+puzzle.complete_puzzle
+=end
+
+puzzle = Sudoku.new('puzzle12.txt')
+puzzle.complete_puzzle
 
 
 
-
-
-
-
-
-
-puzzle = Sudoku.new({"12" => 4, "13" => 5, "14" => 8, "16" => 3, "17" => 7, "18" => 1, "21" => 8, "22" => 1, "28" => 2, "29" => 4, "31" => 7, "33" => 9, "37" => 5, "39" => 8,"44" => 9, "46" => 7,"55" => 6, "64" => 4, "66" => 2, "71" => 6, "73" => 4, "77" => 3, "79" => 5, "81" => 3, "82" => 2, "88" => 8, "89" => 7, "92" => 5, "93" => 7, "94" => 3, "96" => 8, "97" => 2, "98" => 6})
-# print puzzle.check_row(1)
-# print puzzle.check_column(1)
-# print puzzle.check_square(1,1)
-# print puzzle.puzzle
-# puts
-# print puzzle.rotate
-puzzle.display
-puts
-puts 
-puzzle.solve_squares.display
-puts 
-puts
-puzzle.solve_squares.display
-puts 
-puts
-puzzle.solve_squares.display
-puts 
-puts
-puzzle.solve_squares.display
-puts 
-puts
-puzzle.solve_squares.display
-puts 
-puts
-puzzle.solve_squares.display
+# there are puzzles unsolvable by hand that now computer algorithms are being tested to solve. 
